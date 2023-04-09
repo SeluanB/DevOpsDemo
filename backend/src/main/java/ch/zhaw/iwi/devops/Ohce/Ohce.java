80121e384cfb961b79b0a59b7e1d4023a96d75a5
SeluanB <seluan@bluewin.ch>
2023-04-09 12:38:07 +0200

    Verschiebung Methoden zu Ohce.java

--- backend/src/main/java/ch/zhaw/iwi/devops/Ohce/Ohce.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/Ohce/Ohce.java
@@ -1,7 +1,35 @@
 package ch.zhaw.iwi.devops.Ohce;
+import java.time.LocalDateTime;
 
 public class Ohce {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    private String name;
+
+    public Ohce(String name) {
+        this.name = name;
+    }
+
+    public String reverse(String input) {
+        return new StringBuilder(input).reverse().toString();
+    }
+
+    public boolean isPalindrome(String input) {
+        return input.equals(reverse(input));
+    }
+
+    public String greet() {
+        int hour = getCurrentHour();
+        String greeting;
+        if (hour >= 20 || hour < 6) {
+            greeting = "¡Buenas noches " + name + "!";
+        } else if (hour >= 6 && hour < 12) {
+            greeting = "¡Buenos días " + name + "!";
+        } else {
+            greeting = "¡Buenas tardes " + name + "!";
+        }
+        return greeting;
+    }
+
+    public int getCurrentHour() {
+        return LocalDateTime.now().getHour();
     }
 }
--- backend/src/test/java/ch/zhaw/iwi/devops/Ohce/OhceTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/Ohce/OhceTest.java
@@ -1,116 +1,93 @@
 package ch.zhaw.iwi.devops.Ohce;
-
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class OhceTest {
     private String name = "Test";
-
-    // Methode muss in ohce.java
-    private String reverse(String input) {
-        return new StringBuilder(input).reverse().toString();
-    }
-    // Methode muss in ohce.java
-    private boolean isPalindrome(String input) {
-        return input.equals(reverse(input));
-    }
-    // Methode muss in ohce.java
-    private String greet() {
-        int hour = LocalDateTime.now().getHour();
-        String greeting;
-        if (hour >= 20 || hour < 6) {
-            greeting = "¡Buenas noches " + name + "!";
-        } else if (hour >= 6 && hour < 12) {
-            greeting = "¡Buenos días " + name + "!";
-        } else {
-            greeting = "¡Buenas tardes " + name + "!";
-        }
-        return greeting;
-    }
+    private Ohce ohce = new Ohce(name);
 
     @Test
     void reverseTest() {
-        assertEquals("tset", reverse("test"));
-        assertEquals("321", reverse("123"));
-        assertEquals("sdrawkcab", reverse("backwards"));
-        assertEquals("", reverse(""));
-        assertEquals("a", reverse("a"));
+        assertEquals("tset", ohce.reverse("test"));
+        assertEquals("321", ohce.reverse("123"));
+        assertEquals("sdrawkcab", ohce.reverse("backwards"));
+        assertEquals("", ohce.reverse(""));
+        assertEquals("a", ohce.reverse("a"));
     }
 
     @Test
     void reverseSpecialCharactersTest() {
-        assertEquals(")(*&^%$#@!", reverse("!@#$%^&*()"));
+        assertEquals(")(*&^%$#@!", ohce.reverse("!@#$%^&*()"));
     }
 
     @Test
     void reverseUnicodeCharactersTest() {
-        assertEquals("ćiriličan i ćiriličar", reverse("račilirić i nacilirić"));
+        assertEquals("ćiriličan i ćiriličar", ohce.reverse("račilirić i nacilirić"));
     }
 
     @Test
     void isPalindromeTest() {
-        assertTrue(isPalindrome("otto"));
-        assertTrue(isPalindrome("madam"));
-        assertFalse(isPalindrome("hello"));
-        assertFalse(isPalindrome("world"));
-        assertTrue(isPalindrome("a"));
-        assertTrue(isPalindrome(""));
+        assertTrue(ohce.isPalindrome("otto"));
+        assertTrue(ohce.isPalindrome("madam"));
+        assertFalse(ohce.isPalindrome("hello"));
+        assertFalse(ohce.isPalindrome("world"));
+        assertTrue(ohce.isPalindrome("a"));
+        assertTrue(ohce.isPalindrome(""));
     }
 
     @Test
     void isPalindromeSpecialCharactersTest() {
-        assertTrue(isPalindrome("(*&^%$#@!)!@#$%^&*)"));
-        assertFalse(isPalindrome("(*&^%$#@!)!@#$%^&)"));
+        assertTrue(ohce.isPalindrome("(*&^%$#@!)!@#$%^&*)"));
+        assertFalse(ohce.isPalindrome("(*&^%$#@!)!@#$%^&)"));
     }
 
     @Test
     void isPalindromeUnicodeCharactersTest() {
-        assertTrue(isPalindrome("аibоhiоbiа"));
-        assertFalse(isPalindrome("ćiriličan i ćiriličar"));
-    }
-    @Test
-    void greetTest() {
-        String greeting = greet();
-        int hour = LocalDateTime.now().getHour();
-        if (hour >= 20 || hour < 6) {
-            assertEquals("¡Buenas noches " + name + "!", greeting);
-        } else if (hour >= 6 && hour < 12) {
-            assertEquals("¡Buenos días " + name + "!", greeting);
-        } else {
-            assertEquals("¡Buenas tardes " + name + "!", greeting);
-        }
+        assertTrue(ohce.isPalindrome("аibоhiоbiа"));
+        assertFalse(ohce.isPalindrome("ćiriličan i ćiriličar"));
     }
 
     @Test
-    void greetEmptyNameTest() {
-        String emptyName = "";
-        name = emptyName;
-        String greeting = greet();
-        int hour = LocalDateTime.now().getHour();
-        if (hour >= 20 || hour < 6) {
-            assertEquals("¡Buenas noches !", greeting);
-        } else if (hour >= 6 && hour < 12) {
-            assertEquals("¡Buenos días !", greeting);
-        } else {
-            assertEquals("¡Buenas tardes !", greeting);
-        }
-    }
-
-    @Test
-    void greetUnicodeNameTest() {
-        String balkanName = "Иван";
-        name = balkanName;
-        String greeting = greet();
-        int hour = LocalDateTime.now().getHour();
-        if (hour >= 20 || hour < 6) {
-            assertEquals("¡Buenas noches Иван!", greeting);
-        } else if (hour >= 6 && hour < 12) {
-            assertEquals("¡Buenos días Иван!", greeting);
-        } else {
-            assertEquals("¡Buenas tardes Иван!", greeting);
-        }
+void greetTest() {
+    String greeting = ohce.greet();
+    int hour = ohce.getCurrentHour();
+    if (hour >= 20 || hour < 6) {
+        assertEquals("¡Buenas noches " + name + "!", greeting);
+    } else if (hour >= 6 && hour < 12) {
+        assertEquals("¡Buenos días " + name + "!", greeting);
+    } else {
+        assertEquals("¡Buenas tardes " + name + "!", greeting);
     }
 }
+
+@Test
+void greetEmptyNameTest() {
+    Ohce emptyNameOhce = new Ohce("");
+    String greeting = emptyNameOhce.greet();
+    int hour = emptyNameOhce.getCurrentHour();
+    if (hour >= 20 || hour < 6) {
+        assertEquals("¡Buenas noches !", greeting);
+    } else if (hour >= 6 && hour < 12) {
+        assertEquals("¡Buenos días !", greeting);
+    } else {
+        assertEquals("¡Buenas tardes !", greeting);
+    }
+}
+
+@Test
+void greetUnicodeNameTest() {
+    String balkanName = "Иван";
+    Ohce unicodeNameOhce = new Ohce(balkanName);
+    String greeting = unicodeNameOhce.greet();
+    int hour = unicodeNameOhce.getCurrentHour();
+    if (hour >= 20 || hour < 6) {
+        assertEquals("¡Buenas noches Иван!", greeting);
+    } else if (hour >= 6 && hour < 12) {
+        assertEquals("¡Buenos días Иван!", greeting);
+    } else {
+        assertEquals("¡Buenas tardes Иван!", greeting);
+    }
+}
+}
+
+
